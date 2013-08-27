467229d1d9ccd7e3dd5456001d99470a548111e2
dzpqzb <dz@wiz.cn>
2013-08-27 22:09:10 +0800

    数据库路径配置修改

--- DZContacts/settings.py
+++ DZContacts/settings.py
@@ -10,10 +10,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import os
+dbpath = os.path.expanduser('~')
+dbpath = dbpath + '/contacts.db'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/dzpqzb/contacts.db',                      # Or path to database file if using sqlite3.
+        'NAME': dbpath, 
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.

7fda3f85060ff71d32a641024632e86bd0b0a6a9
dzpqzb <dz@wiz.cn>
2013-08-27 22:32:30 +0800

    Revert 743e6d8..5c816e6
    This rolls back to commit 743e6d852f2f0719d283dd4140b911b6a116c900.

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,5 @@
+# Create your views here.
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -12,7 +14,7 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
-        pass
+	pass
 pass
 @csrf_exempt
 def contact_list(request):
@@ -21,14 +23,14 @@ def contact_list(request):
         serializer = DZContactSerializer(contacts)
         return JSONResponse(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DZContactSerializer(data = data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status = 201)
-        else:
-            return JSONResponse(serializer.data, status = 400)
+		data = JSONParser().parse(request)
+		serializer = DZContactSerializer(data = data)
+		if serializer.is_valid():
+			serializer.save()
+			return JSONResponse(serializer.data, status = 201)
+		else:
+			return JSONResponse(serializer.data, status = 400)
 
 def user_avater(request):
-    image_data = open('/Users/dzpqzb/Downloads/avater.jpeg', 'rb').read()
-    return HttpResponse(image_data, mimetype = "image/jpeg")
+	image_data = open('/Users/dzpqzb/Downloads/avater.jpeg', 'rb').read()
+	return HttpResponse(image_data, mimetype = "image/jpeg")
